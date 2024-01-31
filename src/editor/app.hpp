4ef6dfb4451bf44cb8d5a98eb3703310fefa076b
liujuanjuan <liujuanjuan134@foxmail.com>
2024-01-31 14:01:13 +0800

    Hello Triangle (Vulkan-Samples)

---
+++ src/editor/app.hpp
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <platform/application.h>
+
+#include <vulkan/vulkan.hpp>
+
+class PeakApplication : public vkb::Application
+{
+    struct SwapchainData
+    {
+        vk::Extent2D                 extent;                           // The swapchain extent
+        vk::Format                   format = vk::Format::eUndefined;  // Pixel format of the swapchain.
+        vk::SwapchainKHR             swapchain;                        // The swapchain.
+        std::vector<vk::ImageView>   image_views;                      // The image view for each swapchain image.
+        std::vector<vk::Framebuffer> framebuffers;                     // The framebuffer for each swapchain image view.
+    };
+
+    struct FrameData
+    {
+        vk::Fence         queue_submit_fence;
+        vk::CommandPool   primary_command_pool;
+        vk::CommandBuffer primary_command_buffer;
+        vk::Semaphore     swapchain_acquire_semaphore;
+        vk::Semaphore     swapchain_release_semaphore;
+    };
+
+   public:
+    PeakApplication();
+    virtual ~PeakApplication();
+
+   private:
+    // from vkb::Application
+    virtual bool prepare(const vkb::ApplicationOptions &options) override;
+    virtual bool resize(const uint32_t width, const uint32_t height) override;
+    virtual void update(float delta_time) override;
+
+    std::pair<vk::Result, uint32_t> acquire_next_image();
+    vk::Device                      create_device(const std::vector<const char *> &required_device_extensions);
+    vk::Pipeline                    create_graphics_pipeline();
+    vk::ImageView                   create_image_view(vk::Image image);
+    vk::Instance                    create_instance(std::vector<const char *> const &required_instance_extensions, std::vector<const char *> const &required_validation_layers);
+    vk::RenderPass                  create_render_pass();
+    vk::ShaderModule                create_shader_module(const char *path);
+    vk::SwapchainKHR                create_swapchain(vk::Extent2D const &swapchain_extent, vk::SurfaceFormatKHR surface_format, vk::SwapchainKHR old_swapchain);
+    void                            init_framebuffers();
+    void                            init_swapchain();
+    void                            render(uint32_t swapchain_index);
+    void                            select_physical_device_and_surface();
+    void                            teardown_framebuffers();
+    void                            teardown_per_frame(FrameData &per_frame_data);
+
+   private:
+    vk::Instance               instance;               // The Vulkan instance.
+    vk::PhysicalDevice         gpu;                    // The Vulkan physical device.
+    vk::Device                 device;                 // The Vulkan device.
+    vk::Queue                  queue;                  // The Vulkan device queue.
+    SwapchainData              swapchain_data;         // The swapchain state.
+    vk::SurfaceKHR             surface;                // The surface we will render to.
+    uint32_t                   graphics_queue_index;   // The queue family index where graphics work will be submitted.
+    vk::RenderPass             render_pass;            // The renderpass description.
+    vk::PipelineLayout         pipeline_layout;        // The pipeline layout for resources.
+    vk::Pipeline               pipeline;               // The graphics pipeline.
+    vk::DebugUtilsMessengerEXT debug_utils_messenger;  // The debug utils messenger.
+    std::vector<vk::Semaphore> recycled_semaphores;    // A set of semaphores that can be reused.
+    std::vector<FrameData>     per_frame_data;         // A set of per-frame data.
+
+#if defined(VKB_DEBUG) || defined(VKB_VALIDATION_LAYERS)
+    vk::DebugUtilsMessengerCreateInfoEXT debug_utils_create_info;
+#endif
+};
+
+// #include <hpp_api_vulkan_sample.h>
+// class PeakApplication2 : public HPPApiVulkanSample
+//{
+//
+// };
+
+std::unique_ptr<vkb::Application> create_peak_app();
---
+++ src/editor/main.cpp
@@ -0,0 +1,105 @@
+﻿// TODO use c++20 module
+// import vulkan_hpp;
+
+#include <iostream>
+
+// TODO use c++20 designated initializers
+// #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+
+#include "editor/app.hpp"
+
+#include "common/logging.h"
+#include "platform/platform.h"
+#include "plugins/plugins.h"
+
+#include <core/platform/entrypoint.hpp>
+
+#if defined(PLATFORM__ANDROID)
+#	include "platform/android/android_platform.h"
+#elif defined(PLATFORM__WINDOWS)
+#	include "platform/windows/windows_platform.h"
+#elif defined(PLATFORM__LINUX_D2D)
+#	include "platform/unix/unix_d2d_platform.h"
+#elif defined(PLATFORM__LINUX) || defined(PLATFORM__MACOS)
+#	include "platform/unix/unix_platform.h"
+#else
+#	error "Platform not supported"
+#endif
+
+#include <filesystem>
+
+#include "plugins/start_sample/start_sample.h"
+using PeakStartSampleTags = vkb::PluginBase<vkb::tags::Entrypoint>;
+class PeakSample : public PeakStartSampleTags
+{
+	using Super = PeakStartSampleTags;
+public:
+	PeakSample();
+
+	virtual ~PeakSample() = default;
+
+	virtual bool is_active(const vkb::CommandParser& parser) override;
+
+	virtual void init(const vkb::CommandParser& parser) override;
+};
+
+PeakSample::PeakSample() : Super("Peak", "Peak Engine Plugin")
+{
+}
+
+bool PeakSample::is_active(const vkb::CommandParser& parser)
+{
+	return true;
+}
+
+void PeakSample::init(const vkb::CommandParser& parser)
+{
+	{
+		// Launch
+		static apps::AppInfo appInfo = {"Peak",  create_peak_app};
+		{
+			vkb::Window::OptionalProperties properties;
+			std::string                     title = "Peak";
+			properties.title = title;
+			platform->set_window_properties(properties);
+			platform->request_application(&appInfo);
+		}
+	}
+}
+
+CUSTOM_MAIN(context)
+{
+#if defined(PLATFORM__ANDROID)
+	vkb::AndroidPlatform platform{ context };
+#elif defined(PLATFORM__WINDOWS)
+	vkb::WindowsPlatform platform{ context };
+#elif defined(PLATFORM__LINUX_D2D)
+	vkb::UnixD2DPlatform platform{ context };
+#elif defined(PLATFORM__LINUX)
+	vkb::UnixPlatform platform{ context, vkb::UnixType::Linux };
+#elif defined(PLATFORM__MACOS)
+	vkb::UnixPlatform platform{ context, vkb::UnixType::Mac };
+#else
+#	error "Platform not supported"
+#endif
+
+	std::filesystem::current_path(std::filesystem::path(ROOT_SOURCE_DIR));
+
+	PeakSample a;
+
+	auto&& ptrs = plugins::get_all();
+	ptrs.push_back(&a);
+
+	auto code = platform.initialize(ptrs);
+
+	if (code == vkb::ExitCode::Success)
+	{
+		code = platform.main_loop();
+	}
+
+	platform.terminate(code);
+
+	return 0;
+}
+
+
